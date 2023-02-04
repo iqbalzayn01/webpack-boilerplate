a991a687d8a7eafc496f65953413f6766c9ea60e
Iqbal Zayyan <iqbalzayyanabiyyu@gmail.com>
2023-02-04 20:51:34 +0700

    webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,32 @@ module.exports = {
     },
     watch: true,
     devtool: false,
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: [
+                        ['@babel/preset-env'],
+                    ]
+                    }
+                },
+            },
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+        ],
+    },
 };
